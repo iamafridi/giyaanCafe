d5634da4fb459bb9b43a22132f47a4a02bb22b6c
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-09-27 02:13:26 +0600

    added data dynamically

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../blog/blog";
 
 const Blogs = () => {
 const [blogs,setBlogs]=useState([]);
@@ -17,6 +18,13 @@ useEffect(()=>{
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs : {blogs.length}</h1>
+        {
+            blogs.map(blog=>
+                 <Blog key={blog.id} 
+                 blog={blog} ></Blog>)
+        }
+        
+        
         </div>
     );
 };
--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,12 +1,40 @@
 import PropTypes from 'prop-types';
 
-const blog = ({blog}) => {
+const Blog = ({ blog }) => {
     console.log(blog);
+    const { title, cover, author, posted_date, hashtag, author_img, reading_time } = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover pic of ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex' >
+                    <img className='w-14 rounded-full' src={author_img} alt="" />
+                <div className='ml-6'>
+                    <h3 className="text-2xl">{author}</h3>
+               <p>{posted_date}</p>
+               
+                </div>
+
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+{
+    hashtag.map((hash,idx) => <span key={idx}> <a href="">#{hash}</a> </span>)
+}
+
+
+            </p>
         </div>
     );
 };
 
-export default blog;
+Blog.proptypes = {
+    blog: PropTypes.object.isRequired
+}
+
+export default Blog;
