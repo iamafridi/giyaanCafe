90cb14046e96ec648d7115f0c900c1fd976f2a8a
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-09-27 21:33:12 +0600

    added hashtags and fixed the images

--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -9,11 +9,11 @@ const Blog = ({ blog }) => {
             <div className='flex justify-between'>
                 <div className='flex' >
                     <img className='w-14 rounded-full' src={author_img} alt="" />
-                <div className='ml-6'>
-                    <h3 className="text-2xl">{author}</h3>
-               <p>{posted_date}</p>
-               
-                </div>
+                    <div className='ml-6'>
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_date}</p>
+
+                    </div>
 
                 </div>
                 <div>
@@ -23,9 +23,9 @@ const Blog = ({ blog }) => {
 
             <h2 className='text-4xl'>{title}</h2>
             <p>
-{
-    hashtag.map((hash,idx) => <span key={idx}> <a href="">#{hash}</a> </span>)
-}
+                {
+                    hashtag.map((hash, idx) => <span key={idx}> <a href="">#{hash}</a> </span>)
+                }
 
 
             </p>
